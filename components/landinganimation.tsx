55733ddd6c90a923f4e503c69a3523a093ab4ae0
Satish <satish.brainiac@gmail.com>
2025-07-18 22:06:03 +0530

    ???

--- components/landinganimation.tsx
+++ components/landinganimation.tsx
@@ -57,7 +57,7 @@ export default function LandingAnimation() {
 
         gsap.set(".landing-split-overlay .landing-outro-title .char", {
           x: isMobile ? "2em" : "5em",
-          y: isMobile ? "4em" : "2em",
+          y: isMobile ? "2em" : "2em",
         });
 
         const tl = gsap.timeline({ defaults: { ease: "thop" } });
@@ -90,7 +90,7 @@ export default function LandingAnimation() {
           }, 4.5)
           .to(".landing-preloader .landing-outro-title .char", {
             x: isMobile ? "2em" : "5em",
-            y: isMobile ? "4em" : "2em",
+            y: isMobile ? "2em" : "2em",
             duration: 0.75,
             onComplete: () => {
               gsap.set(".landing-preloader", {
