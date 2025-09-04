b6deffe6d1f79b86d9dc16dd92408b8a73007e0e
Satish <satish.brainiac@gmail.com>
2025-09-04 15:46:57 +0530

    blobfish

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
           <p>" Pulkit is a Chihuahua " </p>
         </div>
         <div className="landing-tag landing-tag-3">
-          <p>Sponsored By RAID Shadow Legends</p>
+          <p>Prakhar is a Blobfish</p>
         </div>
       </div>
 
