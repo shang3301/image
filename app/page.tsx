96fe5db2e5df6a3d7d7cffc54b0484605da32d83
Satish <satish.brainiac@gmail.com>
2025-10-27 19:54:34 +0530

    card

--- app/page.tsx
+++ app/page.tsx
@@ -32,10 +32,10 @@ export default function Home() {
           <p>Vasudhaiva Kutumbakam</p>
         </div>
         <div className="landing-tag landing-tag-2">
-          <p>CODE. CREATE. INNOVATE.</p>
+          <p>A Festival of Interschool Competitions</p>
         </div>
         <div className="landing-tag landing-tag-3">
-          <p>IMAGE 2025</p>
+          <p>Novermber 19 & 20, 2025</p>
         </div>
       </div>
 
@@ -49,11 +49,43 @@ export default function Home() {
         </div>
 
         <div className="landing-card">
-          <iframe width="100%" height="100%" 
-          src="https://www.youtube.com/embed/q2UYrRkVIUY" 
-          title="4K 100% Royalty-Free Stock Footage | Technology CPU Light Effect Zooming | No Copyright Video" 
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" >
-          </iframe>
+          <h1>Image 2025</h1>
+          <br />
+          <br />
+          <br />
+          <p>Back with 25 Events</p>
+          <div className="event-lists">
+            <ul className="event-list">
+              <li>Symphony</li>
+              <li>Nrityanjali</li>
+              <li>Sur Sangam</li>
+              <li>Kitchen Geniuses</li>
+              <li>TED Talk</li>
+              <li>बातों–बातों में</li>
+              <li>Poetic Fantasy</li>
+              <li>Sanskrit Shloka</li>
+              <li>Chitrashala</li>
+              <li>Aesthetic Moves</li>
+              <li>GameCraft</li>
+              <li>Webolution</li>
+              <li>CrypteX</li>
+            </ul>
+            <ul className="event-list">
+              <li>ChemCraft 3D</li>
+              <li>Vista View</li>
+              <li>Pulse Within</li>
+              <li>Nukkad Natak</li>
+              <li>Rube It Up!</li>
+              <li>EcoInnovators</li>
+              <li>Reel Harmony</li>
+              <li>GameSpark</li>
+              <li>Top Coders</li>
+              <li>IQrypt</li>
+              <li>Bid Blitz</li>
+              <li>Think Tank</li>
+            </ul>
+          </div>
+            <p className="events-a"><a href="events/">Check The Events Here!</a></p>
         </div>
           <div className="register">
             <p><a href="">Register Here !</a></p>
