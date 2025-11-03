2528b10710f5c0fbba015f144ac5170caaf36dc2
Satish <satish.brainiac@gmail.com>
2025-11-03 22:26:01 +0530

    changes

--- components/menu.jsx
+++ components/menu.jsx
@@ -60,7 +60,7 @@ const Menu = () => {
     if (!container) return;
     container.innerHTML = "";
     const img = document.createElement("img");
-    img.src = "images/6.jpg";
+    img.src = "images/vasu.png";
     img.alt = "";
     container.appendChild(img);
   };
