64eee6d2127b3de8aba0f1b2663cfe2a86ffb5c3
GitSatishGarg <71393132+GitSatishGarg@users.noreply.github.com>
2025-11-04 11:04:51 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -56,16 +56,7 @@ export default function Home() {
 
         {/* Trailer loads after 4 seconds */}
         <div className="landing-card">
-          {showTrailer && (
-            <iframe
-              src="https://player.vimeo.com/video/1132866617?autoplay=1&muted=1&background=1"
-              frameBorder="0"
-              allow="autoplay; fullscreen; picture-in-picture"
-              allowFullScreen
-              title="Image Trailer"
-              style={{ width: "100%", height: "100%" }}
-            ></iframe>
-          )}
+          <img src="images/flex.png" alt="hero" />
         </div>
 
         <div className="register">
